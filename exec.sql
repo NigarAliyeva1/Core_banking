1b3b08e7da2365448162bc548f43e0ef63a985d2
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2023-07-29 23:32:23 +0400

    Create exec.sql

---
+++ exec.sql
@@ -0,0 +1,10 @@
+begin
+    pkg_customers.add_new_customer(p_first_name=>null,
+                                   p_last_name=> null,
+                                   p_father_name=>null,
+                                   p_email=>'ali@gmail.com',
+                                   p_mob_number=>null,
+                                   p_country_name=>'Azerbaijan',
+                                   p_regdate=>null);
+    
+end;   
